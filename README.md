5533e9d4a40d0217337afc03b70035cd53379b3a
pix <tpxsky@163.com>
2025-04-07 14:50:34 +0800

    <add:> readme and kvm install script

---
+++ README.md
@@ -0,0 +1,6 @@
+# ArchLinux Install script
+
+## `kvm_install.sh`
+
+安装`kvm`虚拟机脚本。
+参考`kvm`笔记。
---
+++ kvm_install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+COLOR_GREEN='\033[0;32m'
+COLOR_RED='\033[0;31m'
+COLOR_YELLOW='\033[0;33m'
+COLOR_NC='\033[0m'
+
+function pacman_install() {
+  if pacman -Qi "$1" &>/dev/null; then
+    echo -e "${COLOR_GREEN}$1 is installed${COLOR_NC}"
+  else
+    echo -e "${COLOR_YELLOW}$1 is not install${COLOR_NC}"
+    sudo pacman -S "$1" --noconfirm
+  fi
+}
+
+function trizen_install() {
+  if pacman -Qi "$1" &>/dev/null; then
+    echo -e "${COLOR_GREEN}$1 is installed${COLOR_NC}"
+  else
+    echo -e "${COLOR_YELLOW}$1 is not install${COLOR_NC}"
+    trizen -S "$1" --noconfirm
+  fi
+}
+
+pacman_install qemu-base
+pacman_install libvirt
+pacman_install ovmf
+pacman_install virt-manager
+pacman_install virt-install
+pacman_install bridge-utils
+pacman_install openbsd-netcat
+
+sudo systemctl enable libvirtd
+sudo systemctl start libvirtd
