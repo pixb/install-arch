9d3bfb491a29b776b32a75106e7a4fedeb5a96f1
pix <tpxsky@163.com>
2026-03-20 16:44:02 +0800

    chore: fix script bug.

--- tmux/config_tmux.sh
+++ tmux/config_tmux.sh
@@ -6,7 +6,21 @@
 # Description:
 #########################################################################
 #!/bin/env bash
+COLOR_GREEN='\033[0;32m'
+COLOR_RED='\033[0;31m'
+COLOR_YELLOW='\033[0;33m'
+COLOR_NC='\033[0m'
+
+# Exit when an error occurs
+set -e
+# Throw error when using an undefined variable
+set -u
+
+TIME="$(date +%Y-%m-%d_%H-%M-%S)"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_NAME="$(basename "${BASH_SOURCE[0]}")"
+FILE_NAME="$(basename "${BASH_SOURCE[0]}" .sh)"
 git clone https://github.com/gpakosz/.tmux.git "${HOME}/.tmux"
 ln -s -f "${HOME}/.tmux/.tmux.conf" "${HOME}/.tmux.conf"
 cp "${HOME}/.tmux/.tmux.conf.local" "${HOME}"
-cat "${HOME}/dev/install-arch/tmux/.tmux.conf" >>"${HOME}/.tmux.conf.local"
+cat "${SCRIPT_DIR}/.tmux.conf" >>"${HOME}/.tmux.conf.local"
